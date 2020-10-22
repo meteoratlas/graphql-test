9b39407e62e62e339e15f7dc908f3b1d89b382d9
meteoratlas <kellyaccnts@gmail.com>
2020-10-21 20:50:38 -0600

    basic apollo setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,77 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import {
+    ApolloClient,
+    ApolloProvider,
+    InMemoryCache,
+    gql,
+    useQuery,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://48p1r2roz4.sse.codesandbox.io",
+    cache: new InMemoryCache(),
+});
+
+client
+    .query({
+        query: gql`
+            query GetRates {
+                rates(currency: "USD") {
+                    currency
+                }
+            }
+        `,
+    })
+    .then((result) => console.log(result));
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+                <ExchangeRates />
+            </div>
+        </ApolloProvider>
+    );
+}
+
+const EXCHANGE_RATES = gql`
+    query GetExchangeRates {
+        rates(currency: "USD") {
+            currency
+            rate
+        }
+    }
+`;
+
+function ExchangeRates() {
+    const { loading, error, data } = useQuery(EXCHANGE_RATES);
+
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return data.rates.map(({ currency, rate }) => (
+        <div key={currency}>
+            <p>
+                {currency}: {rate}
+            </p>
+        </div>
+    ));
 }
 
 export default App;
